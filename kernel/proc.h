978ff5416f5f1fbafa960babe9ff7b6a474972ca
ding-hai <hding@freewheel.tv>
2022-03-31 14:55:06 +0800

    lab syscall: trace

--- kernel/proc.h
+++ kernel/proc.h
@@ -92,6 +92,7 @@ struct proc {
   int killed;                  // If non-zero, have been killed
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
+  int tracemask;               // Trace Mask, if the bit is set, we will trace the corresponding sys_call
 
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
